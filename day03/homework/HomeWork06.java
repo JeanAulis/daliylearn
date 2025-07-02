cb23c280027898241f0fbe4de7e1924aba06f909
moon <112243222+JeanAulis@users.noreply.github.com>
2025-07-02 23:47:58 +0800

    chore: 更新项目配置并添加新的作业文件
    - 修改了 .idea/misc.xml 文件中的项目 JDK 名称
    - 新增了多个作业文件：
      - HomeWork02.java：输出从1949年到2088年中所有是猪年的年份 - HomeWork03.java：打印出1988年到2088年的所有闰年年份
      - HomeWork04.java：模拟ATM取款过程的程序
      - HomeWork05.java：打印1到100之内不包含9的整数
      - HomeWork06.java：计算1到指定数字之间的所有奇数和

---
+++ day03/homework/HomeWork06.java
@@ -0,0 +1,37 @@
+package homework;
+
+import java.util.Scanner;
+
+public class HomeWork06 {
+    public static void main(String[] args) {
+//        2.5 编程题目五
+//        需求描述：
+//        键盘录入一个数字, 请计算1~该数字之间, 所有的奇数和, 并将结果打印到控制台上.
+//        例如: 录入:10, 则打印: 1~10之间的奇数和为 25 录入: -10, 则打印: 您录入的数字无效, 请重新录入.
+//        注意: 如果用户录入错误数字, 则一直录入. 直到用户录入正确的数字, 打印完结果后, 程序结束.
+
+        while (true) {
+            try {
+                Scanner sc = new Scanner(System.in);
+                System.out.println("请输入数字：");
+                int num = sc.nextInt();
+                int sum = 0;
+                if (num < 1){
+                    System.out.println("您录入的数字无效, 请重新录入!");
+                }else{
+                    for (int i = 1; i <= num; i++){
+                        if(i % 2 != 0 && i != num){
+                            sum += i;
+                        } else if (i == num) {
+                            sum += i;
+                            System.out.println("1-" + num + "的奇数和为：" + sum);
+                            break;
+                        }
+                    }
+                }
+            }catch (Exception e){
+                System.out.println("请输入合法数字！");
+            }
+        }
+    }
+}
