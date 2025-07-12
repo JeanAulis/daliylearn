890a06d9f8cf61db03300d6965c57fda2001731e
moon <112243222+JeanAulis@users.noreply.github.com>
2025-07-12 15:01:17 +0800

    feat(interface): 添加 JDK 8 新增方法
    - 在 PrintAble 接口中添加了 JDK 8 后新增的默认方法 test1
    - 实现了私有方法 test2 和 test3
    -这些方法用于演示 JDK 8 接口的新特性

--- day07-oop/src/com/itheima/_07接口综合案例多套打印方案/PrintAble.java
+++ day07-oop/src/com/itheima/_07接口综合案例多套打印方案/PrintAble.java
@@ -8,4 +8,21 @@ package com.itheima._07接口综合案例多套打印方案;
 public interface PrintAble {
 
     void PrintStudents(Student[] students);
+
+    // jdk8后新增方法
+    default void test1(){
+        System.out.println("默认行为方法");
+        test2();
+    }
+
+    private void test2(){
+        System.out.println("私有方法");
+        test3();
+    }
+
+    private void test3(){
+        System.out.println("静态方法");
+        test1();
+    }
+
 }
