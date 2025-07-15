b03a9f81ec9c084001dd6169cb83cf13d49173b4
moon <112243222+JeanAulis@users.noreply.github.com>
2025-07-15 10:44:23 +0800

    feat(_04匿名内部类): 添加学生类和比较器实现- 新增 Student 类，包含 name、age 和 score 属性 - 在 Demo05 类中使用匿名内部类实现学生对象的排序- 添加按年龄升序和降序排序的示例

--- day08-oop/src/com/itheima/_04匿名内部类/Student.java
+++ day08-oop/src/com/itheima/_04匿名内部类/Student.java
@@ -1,11 +1,21 @@
 package com.itheima._04匿名内部类;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @Description Student
  * @Author jeanAulis
  * @Date 2025-07-15
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Student implements SwimAble{
+    private String name;
+    private int age;
+    private double score;
 
     /**
      *
