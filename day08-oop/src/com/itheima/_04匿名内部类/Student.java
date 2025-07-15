f33b87e56ebc9559467cf9d42596540f0458ccaf
moon <112243222+JeanAulis@users.noreply.github.com>
2025-07-15 10:08:05 +0800

    feat(day08-oop): 添加匿名内部类相关代码
    - 新增 Demo04 类，演示匿名内部类的使用
    - 新增 Student 类，实现 SwimAble接口
    - 新增 SwimAble 接口，定义 swimming 方法

---
+++ day08-oop/src/com/itheima/_04匿名内部类/Student.java
@@ -0,0 +1,17 @@
+package com.itheima._04匿名内部类;
+
+/**
+ * @Description Student
+ * @Author jeanAulis
+ * @Date 2025-07-15
+ */
+public class Student implements SwimAble{
+
+    /**
+     *
+     */
+    @Override
+    public void swimming() {
+
+    }
+}
